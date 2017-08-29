fabb3fb8465db9792d6b7dffdf952a31f89fbdc0
SimonDanisch <sdanisch@gmail.com>
2017-08-29 09:41:23 +0200

    clean up

--- src/Transpiler.jl
+++ src/Transpiler.jl
@@ -3,11 +3,9 @@ module Transpiler
 
 using StaticArrays, Compat
 using Sugar, DataStructures
-#using GLAbstraction, ModernGL
 
 
 include("clike/shared.jl")
-#include("clike/opengl/compilation.jl")
 include("clike/opencl/printing.jl")
 
 
