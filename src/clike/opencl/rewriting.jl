d76d04b0dd57967db67ac1d17f628eb9d5ad5ee9
SimonDanisch <sdanisch@gmail.com>
2017-09-21 14:03:22 +0200

    fixes for local mem changes

--- src/clike/opencl/rewriting.jl
+++ src/clike/opencl/rewriting.jl
@@ -1,3 +1,4 @@
+extract_val(::Type{Val{X}}) where X = X
 function rewrite_backend_specific{F}(m::CLMethod, f::F, types, expr)
     li = m.parent
     if f == (ifelse) && length(types) == 3 && types[1] == Bool && all(x-> x <: cli.Types || is_fixedsize_array(x), types[2:3])
@@ -16,17 +17,18 @@ function rewrite_backend_specific{F}(m::CLMethod, f::F, types, expr)
         expr.args[2] = :CLK_LOCAL_MEM_FENCE
         return true, expr
     elseif Symbol(f) == Symbol("GPUArrays.LocalMemory")
-        if length(expr.args) != 4
+        if length(expr.args) != 5
             error("LocalMemory needs to be used as: LocalMemory(state, T, N), with T & N being constants / static function parameters")
         end
-        T, N = Sugar.rewrite_ast.(m, expr.args[3:end])
-        if !(isa(T, Type) && isa(N, Integer))
-            error("LocalMemory needs to be used as: LocalMemory(T, N), with T being a type and N an integer")
+        T = Sugar.rewrite_ast.(m, expr.args[3])
+        N = expr.args[4].args[1]
+        if !(isa(T, Type)) #&& (isa(N, Val{<:Integer}) || isa(N, Val{NTuple{N, Integer}} where N)))
+             error("LocalMemory needs to be used as: LocalMemory(T, N), with T being a type and Val{N} where N <: Union{Integer, Tuple}. Found: $T $N")
         end
         name = gensym("localmem")
         ret = Expr(:local_memory, T, name)
         ret.typ = cli.LocalPointer{T}
-        inline = Sugar.InlineNode([Expr(:local_memory_init, T, N, name)], ret)
+        inline = Sugar.InlineNode([Expr(:local_memory_init, T, prod(extract_val(N)), name)], ret)
         return true, inline
     else
         return false, expr
