135e88a6b3cd1231c82b0c55ea14a74995203462
SimonDanisch <sdanisch@gmail.com>
2017-09-22 11:11:30 +0200

    fix tests

--- test/opencl_funcs.jl
+++ test/opencl_funcs.jl
@@ -12,13 +12,13 @@ end
 # empty caches
 Transpiler.empty_caches!()
 
-args = (typeof(+), cli.GlobalPointer{Float32, 1}, cli.GlobalPointer{Float32, 1}, cli.GlobalPointer{Float32, 1})
+args = (typeof(+), cli.GlobalPointer{Float32}, cli.GlobalPointer{Float32}, cli.GlobalPointer{Float32})
 cl_mapkernel = CLMethod((mapkernel, args))
 source = Sugar.getsource!(cl_mapkernel)
-mapsource = """void mapkernel_1(Base123 f, __global float * restrict  a, __global float * restrict  b, __global float * restrict  c)
+mapsource = """void mapkernel_1(Base123 f, __global float *  a, __global float *  b, __global float *  c)
 {
     uint gid;
-    gid = (get_global_id)(0) + (uint){1};
+    gid = get_global_id(0) + (uint)(1);
     float _ssavalue_0;
     _ssavalue_0 = (a)[gid - 0x00000001] + (b)[gid - 0x00000001];
     (c)[gid - 0x00000001] = _ssavalue_0;
@@ -37,7 +37,9 @@ mapsource = """void mapkernel_1(Base123 f, __global float * restrict  a, __globa
         Float32,
         (-, Tuple{UInt32, UInt32}),
         typeof(+),
-        cli.GlobalPointer{Float32,1}
+        cli.GlobalPointer{Float32},
+        (UInt32, Tuple{Int64}),
+        typeof(mapkernel),
     ]
     @test length(deps) == length(deps_test)
     for elem in deps
@@ -48,7 +50,7 @@ end
 #Broadcast
 Base.@propagate_inbounds broadcast_index(arg, shape, i) = arg
 Base.@propagate_inbounds function broadcast_index{T, N}(
-        arg::AbstractArray{T, N}, shape::NTuple{N, Integer}, i
+        arg::cli.GlobalPointer{T}, shape::NTuple{N, Integer}, i
     )
     @inbounds return arg[i]
 end
@@ -69,17 +71,17 @@ function broadcast_kernel(A, f, sz, arg1, arg2)
     return
 end
 
-args = (cli.GlobalPointer{Float32, 1}, typeof(+), Tuple{UInt32}, cli.GlobalPointer{Float32, 1}, Float32)
+args = (cli.GlobalPointer{Float32}, typeof(+), Tuple{UInt32}, cli.GlobalPointer{Float32}, Float32)
 cl_mapkernel = CLMethod((broadcast_kernel, args))
 source = getsource!(cl_mapkernel)
-broadcastsource = """void broadcast_kernel_5(__global float * restrict  A, Base123 f, uint sz, __global float * restrict  arg1, float arg2)
+broadcastsource = """void broadcast_kernel_5(__global float *  A, Base123 f, uint sz, __global float *  arg1, float arg2)
 {
     uint i;
-    i = (get_global_id)(0) + (uint){1};
+    i = get_global_id(0) + (uint)(1);
     ;
-    if(i <= (_prod_2)(sz)){
+    if(i <= _prod_2(sz)){
         float _ssavalue_0;
-        _ssavalue_0 = (broadcast_index_3)(arg1, sz, i) + (broadcast_index_4)(arg2, sz, i);
+        _ssavalue_0 = broadcast_index_3(arg1, sz, i) + broadcast_index_4(arg2, sz, i);
         (A)[i - 0x00000001] = _ssavalue_0;
     };
     ;
@@ -89,6 +91,7 @@ broadcastsource = """void broadcast_kernel_5(__global float * restrict  A, Base1
     @test source == broadcastsource
     deps = dependencies!(cl_mapkernel, true)
     deps_test = [
+        typeof(broadcast_kernel),
         UInt32,
         Int,
         (+,Tuple{UInt32,UInt32}),
@@ -96,13 +99,17 @@ broadcastsource = """void broadcast_kernel_5(__global float * restrict  A, Base1
         (<=,Tuple{UInt32,UInt32}),
         (_prod,Tuple{Tuple{UInt32}}),
         (+,Tuple{Float32,Float32}),
-        (broadcast_index,Tuple{cli.GlobalPointer{Float32,1},Tuple{UInt32},UInt32}),
+        (broadcast_index,Tuple{cli.GlobalPointer{Float32},Tuple{UInt32},UInt32}),
         (broadcast_index,Tuple{Float32,Tuple{UInt32},UInt32}),
         (-,Tuple{UInt32,UInt32}),
-        cli.GlobalPointer{Float32,1},
+        cli.GlobalPointer{Float32},
         typeof(+),
         Tuple{UInt32},
-        Float32
+        Float32,
+        (UInt32, Tuple{Int64}),
+        Bool,
+        typeof(_prod),
+        typeof(broadcast_index)
     ]
     @test length(deps) == length(deps_test)
     for elem in deps
@@ -116,8 +123,8 @@ source = Sugar.getsource!(decl)
 target_source = """float fortest_6(float x)
 {
     float acc;
-    int xtempx_4;
-    int i;
+    long x2temp2;
+    long i;
     acc = x;
     for(i = 1; i <= 5; i++){
         if(i == 1){
@@ -141,35 +148,66 @@ end
 
 source, method, name = Transpiler.kernel_source(custom_index_test, (typeof(1f0*I),))
 source_compare = """// dependencies
+// #custom_index_test
+__constant int FUNC_INST_x2custom_index_test = 0;
+typedef int x2custom_index_test; // empty type emitted as an int
 // UniformScaling{Float32}
 struct  __attribute__ ((packed)) TYPUniformScaling_float{
     float x4;
 };
 typedef struct TYPUniformScaling_float UniformScaling_float;
 
+// Base.#getindex
+__constant int FUNC_INST_Base12getindex = 0;
+typedef int Base12getindex; // empty type emitted as an int
+// Transpiler.CLIntrinsics.#cl_select
+__constant int FUNC_INST_Transpiler1CLIntrinsics12cl_select = 0;
+typedef int Transpiler1CLIntrinsics12cl_select; // empty type emitted as an int
+// Base.Sort.#select
+__constant int FUNC_INST_Base1Sort12select = 0;
+typedef int Base1Sort12select; // empty type emitted as an int
+// (select, Tuple{Float32,Float32,UInt32})
+float select_9(float a, float b, uint c)
+{
+    if((bool)(c)){
+        return a;
+    };
+    return b;
+}
+// (Transpiler.CLIntrinsics.cl_select, Tuple{Float32,Float32,Bool})
+float cl_select_10(float a, float b, bool c)
+{
+    return select_9(a, b, (uint)(c));
+}
 // Symbol
 
+// Base.#zero
+__constant int FUNC_INST_Base12zero = 0;
+typedef int Base12zero; // empty type emitted as an int
+// Base.#oftype
+__constant int FUNC_INST_Base12oftype = 0;
+typedef int Base12oftype; // empty type emitted as an int
 // (oftype, Tuple{Float32,Int64})
-float oftype_9(float x, int c)
+float oftype_11(float x, long c)
 {
     return (float){c};
 }
 // (zero, Tuple{Float32})
 float zero_6(float x)
 {
-    return (oftype_9)(x, 0);
+    return oftype_11(x, 0);
 }
 // (getindex, Tuple{UniformScaling{Float32},Int64,Int64})
-float getindex_7(UniformScaling_float J, int i, int j)
+float getindex_7(UniformScaling_float J, long i, long j)
 {
-    return (select)(J.x4, (zero_6)(J.x4), i == j);
+    return cl_select_10(J.λ, zero_6(J.λ), i == j);
 }
 // ########################
 // Main inner function
 // (custom_index_test, (UniformScaling{Float32},))
 __kernel float custom_index_test_8(UniformScaling_float x)
 {
-    return (getindex_7)(x, 1, 1);
+    return getindex_7(x, 1, 1);
 }
 """
 @test source_compare == source
@@ -181,6 +219,9 @@ end
 
 source, method, name = Transpiler.kernel_source(ntuple_test, (Val{4},))
 compare_source = """// dependencies
+// #ntuple_test
+__constant int FUNC_INST_x2ntuple_test = 0;
+typedef int x2ntuple_test; // empty type emitted as an int
 // Val{4}
 typedef int Val_4; // empty type emitted as an int
 // #inner
@@ -190,30 +231,29 @@ typedef int x2inner; // empty type emitted as an int
 typedef int Type5Val5466; // placeholder type instance
 __constant Type5Val5466 TYP_INST_Type5Val5466 = 0;
 
+// Base.#ntuple
+__constant int FUNC_INST_Base12ntuple = 0;
+typedef int Base12ntuple; // empty type emitted as an int
 // Any
 typedef int Any; // placeholder type instance
 __constant Any TYP_INST_Any = 0;
 
 // (inner, Tuple{Int64})
-float inner_12(int i)
+float inner_14(long i)
 {
-    return (float){i} * 77.0f;
+    return (float)(i) * 77.0f;
 }
 // (ntuple, Tuple{#inner,Type{Val{4}}})
-float4 ntuple_10(x2inner f, Type5Val5466 xunused_3)
+float4 ntuple_12(x2inner f, Type5Val5466 x2unused2)
 {
-    return (float4){(inner_12)(1), (inner_12)(2), (inner_12)(3), (inner_12)(4)};
+    return (float4){inner_14(1), inner_14(2), inner_14(3), inner_14(4)};
 }
-// Type{Val}
-typedef int Type5Val6; // placeholder type instance
-__constant Type5Val6 TYP_INST_Type5Val6 = 0;
-
 // ########################
 // Main inner function
 // (ntuple_test, (Val{4},))
-__kernel float4 ntuple_test_11(Val_4 xunused_2)
+__kernel float4 ntuple_test_13(Val_4 x2unused2)
 {
-    return (ntuple_10)(FUNC_INST_x2inner, TYP_INST_Type5Val5466);
+    return ntuple_12(FUNC_INST_x2inner, TYP_INST_Type5Val5466);
 }
 """
 @test compare_source == source
