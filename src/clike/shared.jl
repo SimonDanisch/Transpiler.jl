14cb8deafd0462b3a9cbaaa844a49cbe161e4b78
SimonDanisch <sdanisch@gmail.com>
2017-12-11 12:07:14 +0100

    norm not intrinsic, fix JuliaGPU/CLArrays.jl#10

--- src/clike/shared.jl
+++ src/clike/shared.jl
@@ -104,7 +104,7 @@ const functions = (
     cos, cospi, cosh, acos, acosh,
     tan, tanh, atan, atanh, atan2,# atanpi, atan2pi, <- julia doesnt have those?!
     max, min,
-    abs, pow, normalize, cross, dot, smoothstep, mix, norm,
+    abs, pow, normalize, cross, dot, smoothstep, mix,
     exp, exp2, exp10, expm1,
     log, log2, log10, log1p,
     length, clamp, fma, fabs, isinf, isnan, sign,
